2b5d5b3a95b4e732715202cfbd6fb45fb042f75e
starkovAE <swix456@gmail.com>
2022-03-28 22:08:43 +0500

    4 commit. Dispatch after - позволяет выполнять блоки кода через определенное время.

--- FirstGCD/SecondViewController.swift
+++ FirstGCD/SecondViewController.swift
@@ -32,7 +32,34 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
 
         fetchImage()
+        delay(3) {
+            self.loginAlert()
+        }
+     
     }
+    private func loginAlert() {
+        let alC = UIAlertController(title: "Зарегистрированы?", message: "Введите ваш логин и пароль", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        
+        alC.addAction(okAction)
+        alC.addAction(cancelAction)
+        
+        alC.addTextField { usernameTF in
+            usernameTF.placeholder = "Введите логин"
+        }
+        alC.addTextField { userPasswordTF in
+            userPasswordTF.placeholder = "Введите пароль"
+        }
+        self.present(alC, animated: true, completion: nil)
+    }
+    
+    private func delay(_ delay: Int, closure: @escaping () -> ()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            closure()
+        }
+    }
+    
     
     private func fetchImage() {
         imageURL = URL(string: "https://www.bmw.ru/content/dam/bmw/marketRU/bmw_ru/images/m2_coupe_1680.jpg/jcr:content/renditions/cq5dam.resized.img.1680.large.time1524218791307.jpg")
